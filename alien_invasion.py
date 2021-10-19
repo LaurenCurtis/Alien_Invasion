6152c562cc83a3e93579dee40031f6b381812484
Lauren <you@example.com>
2021-10-19 11:23:18 -0500

    adding right

--- alien_invasion.py
+++ alien_invasion.py
@@ -16,13 +16,26 @@ class AlienInvasion:
         """Start the main loop for the game"""
         while True:
             #Watch for keyboard and mouse events
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
-            self.screen.fill(self.bg_color)
-            self.ship.blitme()
-            pygame.display.flip()
-
+            self._check_events()
+            self.ship.update()
+            self._update_screen()
+    def _check_events(self):
+        """REsond to keypresses and mouse events"""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    #Move the ship to the right
+                    self.ship.moving_right = True
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = False
+    def _update_screen(self):
+        """Update images on the screen and flip to the new screen"""
+        self.screen.fill(self.bg_color)
+        self.ship.blitme()
+        pygame.display.flip()
 if __name__ == '__main__':
     ai = AlienInvasion()
     ai.run_game()
